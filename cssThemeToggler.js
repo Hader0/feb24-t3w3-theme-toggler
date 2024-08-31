c8d41c92de2419dfc1da8b2d968243122252afc3
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-08-31 10:29:32 +1000

    saving and loading theme working

--- cssThemeToggler.js
+++ cssThemeToggler.js
@@ -2,6 +2,7 @@ console.log("CSS Theme Toggler loaded and running!");
 
 let rootElement = document.querySelector(":root");
 
+// Colour themes for the website
 let themes = [
     {
         name: "dark",
@@ -24,14 +25,35 @@ let themes = [
 // Read theme name stored in local storage
 // Update CSS variables based on that name
 function getChosenTheme() {
-
+    let foundTheme = localStorage.getItem("theme");
+    console.log(foundTheme);
+    return foundTheme;
 }
 
 // Set theme name to local storage
 // Update CSS variables based on that name
-function setChosenTheme() {
-
+function setChosenTheme(newThemeName) {
+    localStorage.setItem("theme", newThemeName);
 }
+
+if (getChosenTheme() == null){
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    if (darkThemeMq.matches) {
+      // Theme set to dark.
+      setChosenTheme("dark");
+      console.log("No theme found, applied the dark theme");
+    } else {
+      // Theme set to light.
+      setChosenTheme("light");
+      console.log("No theme found, applied the light theme");
+    }
+}
+
+
+
+let themeToggleButton = document.getElementById("themeToggle");
+themeToggleButton.onclick = toggleTheme;
+// themeToggleButton.addEventListener("click", toggleTheme); - Same as above
 
 // Loop through properties key in chosen theme object
 // Apple those properties based on that name
